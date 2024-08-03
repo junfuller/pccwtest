59fec2b50d35e43781e4519bdbf01c71bffc1034
JunWick <jguller20223@gmail.com>
2024-08-03 13:19:45 +0800

    update

--- src/main/java/org/example/pccwtest/config/CustomSecurityConfig.java
+++ src/main/java/org/example/pccwtest/config/CustomSecurityConfig.java
@@ -1,19 +1,30 @@
 package org.example.pccwtest.config;
 
+import jakarta.annotation.Resource;
 import lombok.AllArgsConstructor;
+import org.example.pccwtest.security.Anonymous;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @Configuration
 @EnableWebSecurity
 @AllArgsConstructor
 public class CustomSecurityConfig {
-
-    private final PermitAllUrlProperties       permitAllUrl;
+    @Resource
+    private final  RequestMappingHandlerMapping requestMappingHandlerMapping;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -21,14 +32,26 @@ public class CustomSecurityConfig {
                 .csrf( AbstractHttpConfigurer::disable )
                 .authorizeHttpRequests(
                         customizer -> {
-                            //permit all endpoint with @Anonymous
-                            permitAllUrl.getUrls().forEach( url -> customizer.requestMatchers( url ).permitAll() );
+                            Map<RequestMappingInfo, HandlerMethod> map = requestMappingHandlerMapping.getHandlerMethods();
+
+                            final List<String> urls = new ArrayList<>();
+                            map.keySet().forEach(info -> {
+                                HandlerMethod handlerMethod = map.get(info);
+                                Anonymous method = AnnotationUtils.findAnnotation(handlerMethod.getMethod(), Anonymous.class);
+                                Optional
+                                        .ofNullable(method)
+                                        .ifPresent(anonymous -> {
+                                            var patternsCondition = info.getPatternsCondition();
+                                            if(patternsCondition!=null){
+                                                urls.addAll(patternsCondition.getPatterns());
+                                            }
+                                        });
+                            });
+
+                            urls.forEach( url -> customizer.requestMatchers( url ).permitAll() );
 
                             // Permit Swagger API documentation
-                            customizer.requestMatchers("/swagger-ui/**").permitAll();
-                            customizer.requestMatchers("/v3/api-docs/**").permitAll();
-                            customizer.requestMatchers("/swagger-resources/**").permitAll();
-                            customizer.requestMatchers("/webjars/**").permitAll();
+                            customizer.requestMatchers( "/swagger-ui/**", "/v3/api-docs/**", "/*.html", "/*.ico" ).permitAll();
                         }
                 );
         return http.build();
